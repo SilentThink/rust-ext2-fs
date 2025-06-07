403065b718bac4000774b547e13dcc89f7ed482b
NoobDB <t202510145995988@eduxiji.net>
2025-06-07 10:29:21 +0800

    🐛 fix(df): 恢复部分修改

--- src/shell/cmd/df.rs
+++ src/shell/cmd/df.rs
@@ -15,8 +15,8 @@ impl Df {
         let mut size = bytes as f64;
         let mut unit_index = 0;
 
-        while size >= BLOCK_SIZE as f64 && unit_index < UNITS.len() - 1 {
-            size /= BLOCK_SIZE as f64;
+        while size >= 1024.0 && unit_index < UNITS.len() - 1 {
+            size /= 1024.0;
             unit_index += 1;
         }
 
